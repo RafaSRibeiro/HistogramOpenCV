69888e424256c46d9b0d2adb520ecef97f5fe883
Rafael Ribeiro <rafael.s.ribeiro@sc.senai.br>
2020-05-19 18:19:39 -0300

    Init Commit

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        // load the native OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        launch(args);
+    }
+}
